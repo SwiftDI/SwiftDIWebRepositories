dae06c7115db0212b69d149313ec8e009bf83c67
Alex Basson <alexbasson@gmail.com>
2017-02-06 10:14:52 -0500

    Remove allTests() from test files

--- README.md
+++ README.md
@@ -52,16 +52,6 @@ import SwiftDIHLP
 
 class MyNewRepositoryTests: GameRepositoryTests {
 
-    static var allTests: [(String, (MyNewRepositoryTests) -> () throws -> Void)] {
-        return [
-            ("testSave_callsTheCompletionWithTheSavedGames", testSave_callsTheCompletionWithTheSavedGames),
-            ("testFetchGames_whenThereAreNoSavedGames_callsTheCompletionWithEmptyArray", testFetchGames_whenThereAreNoSavedGames_callsTheCompletionWithEmptyArray),
-            ("testFetchGames_whenThereIsASavedGame_callsTheCompletionWithTheGame", testFetchGames_whenThereIsASavedGame_callsTheCompletionWithTheGame),
-            ("testFetchGameById_whenThereIsNoGameWithThatId_callsTheCompletionWithNil", testFetchGameById_whenThereIsNoGameWithThatId_callsTheCompletionWithNil),
-            ("testFetchGameById_whenThereIsAGameWithTheId_callsTheCompletionWithTheFetchedGame", testFetchGameById_whenThereIsAGameWithTheId_callsTheCompletionWithTheFetchedGame)
-        ]
-    }
-
     override func setUp() {
         gameRepository = MyNewRepository()
         super.setUp()
--- Tests/SwiftDIWebRepositoriesTests/Repositories/InMemoryGameRepositoryTests.swift
+++ Tests/SwiftDIWebRepositoriesTests/Repositories/InMemoryGameRepositoryTests.swift
@@ -4,16 +4,6 @@ import SwiftDIHLP
 
 class InMemoryGameRepositoryTests: GameRepositoryTests {
 
-    static var allTests: [(String, (InMemoryGameRepositoryTests) -> () throws -> Void)] {
-        return [
-            ("testSave_callsTheCompletionWithTheSavedGames", testSave_callsTheCompletionWithTheSavedGames),
-            ("testFetchGames_whenThereAreNoSavedGames_callsTheCompletionWithEmptyArray", testFetchGames_whenThereAreNoSavedGames_callsTheCompletionWithEmptyArray),
-            ("testFetchGames_whenThereIsASavedGame_callsTheCompletionWithTheGame", testFetchGames_whenThereIsASavedGame_callsTheCompletionWithTheGame),
-            ("testFetchGameById_whenThereIsNoGameWithThatId_callsTheCompletionWithNil", testFetchGameById_whenThereIsNoGameWithThatId_callsTheCompletionWithNil),
-            ("testFetchGameById_whenThereIsAGameWithTheId_callsTheCompletionWithTheFetchedGame", testFetchGameById_whenThereIsAGameWithTheId_callsTheCompletionWithTheFetchedGame)
-        ]
-    }
-
     override func setUp() {
         gameRepository = InMemoryGameRepository()
         super.setUp()
