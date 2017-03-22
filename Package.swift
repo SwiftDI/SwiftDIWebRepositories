4e0bf62c7c463009e77bca4dd13e24a1ebf18829
Alex Basson <alexbasson@gmail.com>
2017-03-21 20:30:06 -0400

    Update Package.swift to point to new repositories

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftDIWebRepositories",
     dependencies: [
-        .Package(url: "https://github.com/alexbasson/SwiftDIHLP.git", majorVersion: 0),
+        .Package(url: "https://github.com/SwiftDI/SwiftDIHLP.git", majorVersion: 0),
         .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 1)
     ]
 )
